659929ee76c60d61e656b48ac44e3fd735572e0b
parth.pandya@valensdatalabs.com <parth.pandya@rackspace.com>
2023-02-08 16:00:30 +0530

    Adds exercise 3

--- util.py
+++ util.py
@@ -6,4 +6,4 @@ def get_spark_session():
         .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
         .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
 
-    return configure_spark_with_delta_pip(builder).getOrCreate()
+    return configure_spark_with_delta_pip(builder, ["com.databricks:spark-xml_2.12:0.16.0"]).getOrCreate()
