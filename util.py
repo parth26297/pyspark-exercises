e381a066b9449ff4913a18c29f17fee5409e567a
parth.pandya@valensdatalabs.com <parth.pandya@rackspace.com>
2023-03-16 11:43:23 +0530

    Reset env

--- util.py
+++ util.py
@@ -1,9 +1,11 @@
-import pyspark
+from pyspark.sql import SparkSession
 from delta import *
 
 def get_spark_session():
-    builder = pyspark.sql.SparkSession.builder.appName("SparkExercisesApp") \
+    builder = SparkSession.builder.appName("SparkExercisesApp") \
         .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
-        .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+        .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
+        .config("spark.sql.warehouse.dir", "spark-warehouse") \
+        .enableHiveSupport()
 
     return configure_spark_with_delta_pip(builder, ["com.databricks:spark-xml_2.12:0.16.0"]).getOrCreate()
